84275d12cb387c0c7f66d8c4ba39a86e31210c40
AdrianRaducanu <48106661+AdrianRaducanu@users.noreply.github.com>
2023-01-26 22:30:00 +0200

    2. Basic style - create components using CLI

--- memo-app/src/app/app.module.ts
+++ memo-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
+import { LevelsComponent } from './components/levels/levels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent,
+    LevelsComponent
   ],
   imports: [
     BrowserModule
